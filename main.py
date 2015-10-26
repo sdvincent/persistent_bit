eadf700c2ab658e9800fe1815bff80a441d72c4f
Jared Stroud <jaredestroud@gmail.com>
2015-10-26 18:57:47 -0400

    raw_input replaced input. This prevents EOF error

--- main.py
+++ main.py
@@ -36,8 +36,10 @@ if __name__ == "__main__":
 
     print(TermColors.blue + "Goodbye World")
     while True:
+
         try:
-            x = input(">> ")
+            x = raw_input(">> ")
+
         except NameError as cerr:
             print(TermColors.red + "Sorry, I didn't understand " + str(cerr))
             print(TermColors.blue) # Go back to blue as default terminal color.
