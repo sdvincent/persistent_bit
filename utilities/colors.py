219008dd6391a5c5cc3375272a84ec586854fac6
Scott Vincent <Scott@Scotts-MacBook-Pro.local>
2015-11-05 02:02:03 -0500

    Added more colors. Release the rainbow.

--- utilities/colors.py
+++ utilities/colors.py
@@ -6,6 +6,12 @@
 '''
 class TermColors:
 
-    red = "\033[91m" 
+    grey = '\033[90m'
+    red = '\033[91m"'
+    green = '\033[92m'
+    yellow = '\033[93m'
     blue = "\033[94m"
-    green = "\033[92m"
+    magenta = '\033[95m'
+    cyan = '\033[96m'
+    white = '\033[97m'
+    default = '\033[99m'
