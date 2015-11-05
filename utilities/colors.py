a0ea93a3aeb3a70b3ffa9225849b1d3ba57c3936
Scott Vincent <Scott@Scotts-MacBook-Pro.local>
2015-11-05 02:12:03 -0500

    Had extra quote, fixed.

--- utilities/colors.py
+++ utilities/colors.py
@@ -7,10 +7,10 @@
 class TermColors:
 
     grey = '\033[90m'
-    red = '\033[91m"'
+    red = '\033[91m'
     green = '\033[92m'
     yellow = '\033[93m'
-    blue = "\033[94m"
+    blue = '\033[94m'
     magenta = '\033[95m'
     cyan = '\033[96m'
     white = '\033[97m'
